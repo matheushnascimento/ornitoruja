fffe9321baebe476278700bf1e48fa09433902d6
Matheus Nascimento <89102783+matheushnascimento@users.noreply.github.com>
2024-04-21 19:25:43 +0000

    adicionar arquivo `.prettierignore`

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <h1>:3 Esse pedacinho de terreno da internet é 100% dedicado à você :3</h1>;
+  <h1>:3 Esse pedacinho de terreno da internet é 100% dedicado à você :3</h1>;
+}
+
+function teste() {
+  console.log("teste");
 }
