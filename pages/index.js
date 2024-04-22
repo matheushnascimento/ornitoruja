98dba1663cdd71119ac3dfdcbb8ba2b2532f2b64
Matheus Nascimento <89102783+matheushnascimento@users.noreply.github.com>
2024-04-22 20:26:00 +0000

    bug:função Home estava sem `return`

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
 export default function Home() {
-  <h1>:3 Esse pedacinho de terreno da internet é 100% dedicado à você :3</h1>;
-}
-
-function teste() {
-  console.log("teste");
+  return (
+    <h1>:3 Esse pedacinho de terreno da internet é 100% dedicado à você :3</h1>
+  );
 }
