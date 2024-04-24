9cfe1303cf886b2654cd0be5a9551c9da5ac5875
Matheus Nascimento <89102783+matheushnascimento@users.noreply.github.com>
2024-04-24 11:54:56 +0000

    mak `database.js` `client.connect()` more robust to errors

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 export default function Home() {
   return (
-    <h1>
-      &#128526; Esse pedacinho de terreno da internet é 100% dedicado à você
-      &#128526;
-    </h1>
+    <div>
+      <h1>
+        &#128526; Esse pedacinho de terreno da internet é 100% dedicado à você
+        &#128526;
+      </h1>
+    </div>
   );
 }
