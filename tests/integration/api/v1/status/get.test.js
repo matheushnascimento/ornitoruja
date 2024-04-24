87484bc62b61aa70008b009beb619c04ee819229
Matheus Nascimento <89102783+matheushnascimento@users.noreply.github.com>
2024-04-24 01:53:31 +0000

    finish `api/v1/status` endpoint

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,19 @@
 test("GET to /api/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  expect(responseBody.updated_at).toBeDefined();
+  expect(responseBody.version).toBeDefined();
+  expect(responseBody.max_connections).toBeDefined();
+  expect(responseBody.open_connections).toBeDefined();
+
+  const parseUpdatedAt = new Date(responseBody.updated_at).toISOString();
+
+  expect(responseBody.updated_at).toEqual(parseUpdatedAt);
+  expect(responseBody.version).toBe("16.0");
+  expect(typeof responseBody.max_connections).toBeTruthy();
+  expect(typeof responseBody.open_connections).toBeTruthy();
+  expect(responseBody.open_connections).toEqual(1);
 });
