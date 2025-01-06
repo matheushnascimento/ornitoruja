daaebb99f32ea7eac8ac1266efcf517912ad8ff3
matheushnascimento <matheus.mhddn@gmail.com>
2025-01-06 19:04:04 -0300

    feat: create and implement `InternalServerError`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,26 +1,37 @@
 import database from "infra/database.js";
+import { InternalServerError } from "infra/errors";
+
 export default async function status(req, response) {
-  const databaseName = process.env.POSTGRES_DB;
-  //#region Fetch database status
-  const updatedAt = new Date().toISOString();
-  const versionQuery = await database.query("SHOW server_version;");
-  const openConnectionsQuery = await database.query({
-    text: "SELECT count(*)::int AS open_connections FROM pg_stat_activity WHERE datname = $1;",
-    values: [databaseName],
-  });
-  const maxConnectionsQuery = await database.query("SHOW max_connections;");
-  //#endregion
+  try {
+    const databaseName = process.env.POSTGRES_DB;
+    //#region Fetch database status
+    const updatedAt = new Date().toISOString();
+    const versionQuery = await database.query("SHOW server_version;");
+    const openConnectionsQuery = await database.query({
+      text: "SELECT count(*)::int AS open_connections FROM pg_stat_activity WHERE datname = $1;",
+      values: [databaseName],
+    });
+    const maxConnectionsQuery = await database.query("SHOW max_connections;");
+    //#endregion
 
-  const queriesResponses = {};
+    const queriesResponses = {};
 
-  //#region QueriesResponse assignments
-  queriesResponses.updated_at = updatedAt;
-  queriesResponses.version = versionQuery.rows[0].server_version;
-  queriesResponses.max_connections = parseInt(
-    maxConnectionsQuery.rows[0].max_connections,
-  );
-  queriesResponses.open_connections =
-    openConnectionsQuery.rows[0].open_connections;
-  //#endregion
-  response.status(200).json(queriesResponses);
+    //#region QueriesResponse assignments
+    queriesResponses.updated_at = updatedAt;
+    queriesResponses.version = versionQuery.rows[0].server_version;
+    queriesResponses.max_connections = parseInt(
+      maxConnectionsQuery.rows[0].max_connections,
+    );
+    queriesResponses.open_connections =
+      openConnectionsQuery.rows[0].open_connections;
+    //#endregion
+    response.status(200).json(queriesResponses);
+  } catch (error) {
+    console.log("\n Erro dentro do catch do controller");
+    const publicErrorObject = new InternalServerError({
+      cause: error,
+    });
+    console.error(publicErrorObject);
+    response.status(500).json(publicErrorObject);
+  }
 }
